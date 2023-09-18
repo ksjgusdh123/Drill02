cb67118d9f3fba81ccd744f6fdcdc7e0a15638cd
hyundojeong <122209567+ksjgusdh123@users.noreply.github.com>
2023-09-18 12:13:54 +0900

    캐릭터 원운동 완성

--- character_grass.py
+++ character_grass.py
@@ -1,45 +1,21 @@
 from pico2d import *
-
+import math
 open_canvas()
 
 # fill here
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-x = 0
+x = 400
 y = 90
-while x >= 0:
-    if x < 750:
-        if y == 90:
-            while(x < 750):
-                clear_canvas_now()
-                grass.draw_now(400,30)
-                character.draw_now(x,y)
-                x = x + 2
-                delay(0.01)
-        elif y > 90:
-            while (y > 90):
-                clear_canvas_now()
-                grass.draw_now(400,30)
-                character.draw_now(x,y)
-                y = y - 2
-                delay(0.01)
-    
+angle = 0
 
-    elif x >= 750:
-        if(y < 550):
-            while(y < 550):
-                clear_canvas_now()
-                grass.draw_now(400,30)
-                character.draw_now(750,y)
-                y = y + 2
-                delay(0.01)
-        elif (y >= 550):
-            while(x > 0):
-                clear_canvas_now()
-                grass.draw_now(400,30)
-                character.draw_now(x,y)
-                x = x - 2
-                delay(0.01)
-  
+while 1:
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(x,y)
+    x = x + math.cos(angle) * 100
+    y = y + math.sin(angle) * 100
+    angle = angle + 1
+    delay(0.1)
 close_canvas()
